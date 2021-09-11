f08c401a584f3942d0d396d3b1ade985f212aa5f
Francesco Pastore <francesco0599@hotmail.it>
2021-09-11 15:05:01 +0200

    Updated title section

---
+++ components/CardProject.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card class="mx-auto">
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title>{{ project.title }}</v-list-item-title>
+        <v-list-item-subtitle>{{ project.description }}</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80">
+        <v-img :src="project.image" height="100px" contain rounded></v-img>
+      </v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-icon v-for="icon in project.icons" :key="icon" class="ma-2">{{
+        icon
+      }}</v-icon>
+    </v-card-actions>
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-btn color="primary" large text>
+        <v-icon left>mdi-open-in-new</v-icon>
+        View
+        <v-icon right>mdi-chevron-right</v-icon>
+      </v-btn>
+
+      <v-btn color="primary" large text @click="openLink(project.url)">
+        <v-icon left>mdi-github</v-icon>
+        Source code
+        <v-icon right>mdi-chevron-right</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    openLink(link) {
+      window.open(link)
+    },
+  },
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,42 @@
 <template>
   <div>
     <the-header></the-header>
-    <v-container>
-      <h1 class="text-center">Projects</h1>
-      <br />
-      <br />
-      <v-row justify="center" align="center">
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-      </v-row>
-      <v-row justify="center" align="center">
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-      </v-row>
-    </v-container>
-
-    <v-container>
-      <h1 class="text-center">Articles</h1>
-      <br />
-      <br />
-      <v-row justify="center" align="center">
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-      </v-row>
-      <v-row justify="center" align="center">
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-        <v-col cols="12" md="3">
-          <app-card></app-card>
-        </v-col>
-      </v-row>
+    <v-container fluid>
+      <v-col cols="12" md="8" offset-md="2">
+        <h1 class="text-center primary--text my-6" style="font-size: 2.5em">
+          Projects
+        </h1>
+        <v-row justify="center" align="center">
+          <v-col
+            v-for="project in projects"
+            :key="project.title"
+            cols="12"
+            md="4"
+          >
+            <card-project :project="project"></card-project>
+          </v-col>
+        </v-row>
+      </v-col>
     </v-container>
   </div>
 </template>
 
 <script>
 import TheHeader from '~/components/TheHeader.vue'
+import Projects from '~/assets/projects.json'
 
 export default {
   components: { TheHeader },
+  data() {
+    return {
+      projects: Projects,
+    }
+  },
 }
 </script>
 
 <style>
+.title {
+  font-size: 20em;
+}
 </style>

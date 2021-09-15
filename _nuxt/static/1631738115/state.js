3b5abf4fd8a6f25f9874cfdf79edd48c41111dc9
francescopastore <francescopastore@users.noreply.github.com>
2021-09-15 20:35:17 +0000

    deploy: 8f744bd195d4c5b16084f523c260db71460df840

---
+++ _nuxt/static/1631738115/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Ffrancescopastore.github.io\u002F_nuxt\u002Fstatic\u002F1631738115",layout:"default",error:a,state:{settings:{theme:{dark:b}}},serverRendered:b,routePath:"\u002F",config:{_app:{basePath:"\u002Ffrancescopastore.github.io\u002F",assetsPath:"\u002Ffrancescopastore.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null,true));

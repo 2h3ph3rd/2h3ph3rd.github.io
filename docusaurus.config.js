2d487b799d2fc49e635ad48c0bff49f0057420cb
Francesco Pastore <francesco0599@hotmail.it>
2023-03-27 18:11:53 +0200

    docs(general): :speech_balloon: Update tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Francesco Pastore',
-  tagline: 'Francesco Pastore website',
+  tagline: 'My personal website',
   favicon: 'img/favicon.ico',
 
   url: 'https://2h3ph3rd.github.io',

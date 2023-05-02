68b869912b7d1b5248c2921ad46d0ebfe74bae9a
--global <francesco0599@hotmail.it>
2023-05-02 21:54:23 +0200

    feat(general): :heavy_plus_sign: Add FontAwesome

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -4,9 +4,17 @@ import MDXComponents from '@theme-original/MDXComponents'
 import Gist from 'react-gist'
 import Image from '@site/src/components/Image'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, fas)
+
 export default {
   // Re-use the default mapping
   ...MDXComponents,
   Gist,
   Image,
+  Icon: FontAwesomeIcon,
 }

08f09d28eb967ccfbcebc56be0226f7e88f36840
--global <francesco0599@hotmail.it>
2024-03-23 18:39:42 +0100

    feat: :sparkles: Add caption and height fields

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
-export default function ({ src, width }) {
+export default function ({ children, src, width, height }) {
+  let caption = '';
   if (width == undefined) {
     width = 'auto'
   }
+
+  if (height == undefined) {
+    height = 'auto'
+  }
+
+  if (children) {
+    caption = <p style={{ fontSize: '0.8em' }}>{children}</p>
+  }
+
   return (
     <p align="center">
-      <img src={src} width={width}></img>
+      <img src={src} width={width} height={height}></img>
+      {caption}
     </p>
   )
 }

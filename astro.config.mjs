24969d37c8dc33e6ec6e62e01b831ab76171f949
--global <francesco0599@hotmail.it>
2025-08-13 10:45:50 +0200

    feat(project): :heavy_plus_sign: Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://2h3ph3rd.github.io',
   base: '',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });

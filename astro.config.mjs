7261ed139f870f602a4d96f34723c0106230b738
--global <francesco0599@hotmail.it>
2025-08-13 09:56:49 +0200

    feat(actions): :construction_worker: Add Github Action yml files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://2h3ph3rd.github.io',
+  base: '',
+});
